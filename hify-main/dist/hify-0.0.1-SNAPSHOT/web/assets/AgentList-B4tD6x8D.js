e61cd46b828d13b510c95cdc1feb27189b5440b5
赵顺畅 <3127532565@qq.com>
2026-07-17 11:00:18 +0800

    chore: remove generated artifacts from repository

--- hify-main/dist/hify-0.0.1-SNAPSHOT/web/assets/AgentList-B4tD6x8D.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,b as c}from"./index-DxkwyyXp.js";const n={};function r(o,s){return c(),t("div",null,"Agent 管理")}const f=e(n,[["render",r]]);export{f as default};
--- hify-main/hify-web/dist/assets/ChatView-BnR-1IMP.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,j as c,c as t}from"./index-SDI-gMK6.js";const n={};function r(o,a){return c(),t("div",null,"对话")}const _=e(n,[["render",r]]);export{_ as default};
